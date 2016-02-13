84b7bf60df15540f8552fa04544fa0fbe17efe3c
Asya Bakhnar <katana8832@yandex.ru>
2016-02-13 20:43:05 +0300

    not work commit

---
+++ src/AppBundle/Entity/CellValue.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="cell_values")
+ */
+class CellValue extends BaseEntity{
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $value_float;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $value_string;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $value;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Table",inversedBy="values")
+     * @ORM\JoinColumn(name="table_id", referencedColumnName="id")
+     */
+    private $table;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Column",inversedBy="values")
+     * @ORM\JoinColumn(name="col_id", referencedColumnName="id")
+     */
+    private $column;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Row", mappedBy="value", cascade={"all"}, orphanRemoval=true, fetch="LAZY")
+     */
+    private $rows;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->rows = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Set value_float
+     *
+     * @param boolean $valueFloat
+     * @return Value
+     */
+    public function setValueFloat($valueFloat)
+    {
+        $this->value_float = $valueFloat;
+
+        return $this;
+    }
+
+    /**
+     * Get value_float
+     *
+     * @return boolean 
+     */
+    public function getValueFloat()
+    {
+        return $this->value_float;
+    }
+
+    /**
+     * Set value_string
+     *
+     * @param boolean $valueString
+     * @return Value
+     */
+    public function setValueString($valueString)
+    {
+        $this->value_string = $valueString;
+
+        return $this;
+    }
+
+    /**
+     * Get value_string
+     *
+     * @return boolean 
+     */
+    public function getValueString()
+    {
+        return $this->value_string;
+    }
+
+    /**
+     * Set table
+     *
+     * @param \AppBundle\Entity\Table $table
+     * @return Value
+     */
+    public function setTable(\AppBundle\Entity\Table $table = null)
+    {
+        $this->table = $table;
+
+        return $this;
+    }
+
+    /**
+     * Get table
+     *
+     * @return \AppBundle\Entity\Table 
+     */
+    public function getTable()
+    {
+        return $this->table;
+    }
+
+
+    /**
+     * Add rows
+     *
+     * @param \AppBundle\Entity\Row $rows
+     * @return Value
+     */
+    public function addRow(\AppBundle\Entity\Row $rows)
+    {
+        $this->rows[] = $rows;
+
+        return $this;
+    }
+
+    /**
+     * Remove rows
+     *
+     * @param \AppBundle\Entity\Row $rows
+     */
+    public function removeRow(\AppBundle\Entity\Row $rows)
+    {
+        $this->rows->removeElement($rows);
+    }
+
+    /**
+     * Get rows
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getRows()
+    {
+        return $this->rows;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     * @return Value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set column
+     *
+     * @param \AppBundle\Entity\Column $column
+     * @return CellValue
+     */
+    public function setColumn(\AppBundle\Entity\Column $column = null)
+    {
+        $this->column = $column;
+
+        return $this;
+    }
+
+    /**
+     * Get column
+     *
+     * @return \AppBundle\Entity\Column 
+     */
+    public function getColumn()
+    {
+        return $this->column;
+    }
+}
