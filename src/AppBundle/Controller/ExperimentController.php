8c2f66842979329d19fbb7d3a08f26b89ec95636
Asya Bakhnar <katana8832@yandex.ru>
2016-03-16 20:55:33 +0300

    add opacity-box

--- src/AppBundle/Controller/ExperimentController.php
+++ src/AppBundle/Controller/ExperimentController.php
@@ -53,8 +53,8 @@ class ExperimentController extends Controller
         $em->persist($exp);
         $em->flush();
 
-        return $this->redirectToRoute('aboutExperiment', array('id' => $exp->getId()));
-//        return $this->redirectToRoute('userExperiment', array('id' => $user-> getId(), 'experimentId' => $exp->getId()) );
+//        return $this->redirectToRoute('aboutExperiment', array('id' => $exp->getId()));
+        return $this->redirectToRoute('userExperiment', array('id' => $user->getId(), 'experimentId' => $exp->getId()) );
     }
 
     /**
